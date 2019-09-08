00949736fe602bd96a7dc9ccd8505f21eaaa759a
Sean Fisher <sean999fisher@gmail.com>
2019-09-08 12:33:04 -0400

    Dialogue, signs, start animal/balloon, etc.

---
+++ Assets/Scripts/Interactables/Sign.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sign : Interactable
+{
+    public string[] dialogueToDisplay;
+    public bool stopsPlayerWhileReading = true;
+
+    public override void StartInteraction(GameObject interactor)
+    {
+        base.StartInteraction(interactor);
+
+        PlayerCharacter pc = interactor.GetComponent<PlayerCharacter>();
+        if (pc)
+        {
+            pc.controlDisabled = true;
+        }
+
+        GameManager.GetMainUI().DisplayDialogue(dialogueToDisplay);
+        GameManager.GetMainUI().onFinishDialogue += EndInteraction;
+    }
+
+    public override void EndInteraction()
+    {
+        base.EndInteraction();
+
+        PlayerCharacter pc = objectThatWeAreInteractingWith.GetComponent<PlayerCharacter>();
+        if (pc)
+        {
+            pc.controlDisabled = false;
+        }
+    }
+}
