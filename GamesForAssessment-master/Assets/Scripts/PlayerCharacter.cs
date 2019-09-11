5a308d47b2445249a753fd916e28d24b60549f60
tessastubbs <stubbs.tessa@yahoo.com>
2019-09-11 13:05:28 -0400

    UPT: Tessa changes

---
+++ GamesForAssessment-master/Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    public float maxSpeed;
+
+    Rigidbody2D rb;
+    Vector2 movement;
+
+    void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if (input.magnitude > 1) {
+            input = input.normalized;
+        }
+
+        movement = input * maxSpeed;
+    }
+
+    void FixedUpdate() {
+
+        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+    }
+}
