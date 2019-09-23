d8212e928f76902aeaa6a4b16ee30edc17ed5015
Sranee Bayapureddy <sbayapureddy6@gatech.edu>
2019-09-22 23:17:08 -0400

    yeet

--- Assets/Bhicken.cs
+++ Assets/Bhicken.cs
@@ -2,23 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//public float speed;
+
+
+// Start is called before the first frame update
+
+
+//// Update is called once per frame
+//void Update()
+//{
+//    transform.position += Time.deltaTime * new Vector3(speed, 0, 0); //every frame add one unit on the x to position
+
+
+//}
+
 public class Bhicken : MonoBehaviour
 {
-    public float speed;
-    
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    private bool dirRight = true;
+    public float speed = 2.0f;
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        transform.position += Time.deltaTime * new Vector3(speed, 0, 0); //every frame add one unit on the x to position
+        if (dirRight)
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        else
+            transform.Translate(-Vector2.right * speed * Time.deltaTime);
 
+        if (transform.position.x >= 4.0f)
+        {
+            dirRight = false;
+        }
 
-
+        if (transform.position.x <= -4)
+        {
+            dirRight = true;
+        }
     }
 }
