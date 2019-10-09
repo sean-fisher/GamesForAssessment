928953a5cff00b32043f63e41cd723b9b8548c39
pbdomingo <patriciabdomingo@gmail.com>
2019-10-09 13:36:34 -0400

    clickable bulletin board takes you to different scenes

---
+++ Assets/SceneSwitcher.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+
+    //public string SceneToLoad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //public void GoToScene()
+    //{
+    //    if (SceneToLoad != "")
+    //    {
+    //        SceneManager.LoadScene(SceneToLoad);
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("Scene has not been set!");
+    //    }
+
+    //}
+
+
+
+    public void GotoShopScene()
+    {
+        SceneManager.LoadScene("Shop");
+    }
+
+    public void GotoForestRiverScene()
+    {
+        SceneManager.LoadScene("ForestRiver");
+    }
+
+    public void GotoForestScene()
+    {
+        SceneManager.LoadScene("ForestScene");
+    }
+
+    public void GotoSbRoomScene()
+    {
+        SceneManager.LoadScene("SbRoom");
+    }
+
+    public void GotoParkScene()
+    {
+        SceneManager.LoadScene("Park");
+    }
+}
