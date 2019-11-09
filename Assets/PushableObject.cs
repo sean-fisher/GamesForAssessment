ff64fd7342feaaa9ec7047b4c4dcc9bb9b951a14
Michael Ditto <michael.r.ditto@gmail.com>
2019-11-09 17:11:30 -0500

    UPT: Scene reorganizing, script cleanup

--- Assets/PushableObject.cs
+++ Assets/PushableObject.cs
@@ -18,34 +18,25 @@ public class PushableObject : MonoBehaviour {
         if (other.gameObject.GetComponent<PlayerCharacter>()) {
             var selfPosition = this.transform.position;
             var otherPosition = coll.transform.position;
-
             var delta = selfPosition - otherPosition;
-            LayerMask mask = LayerMask.GetMask("Default");
+            var mask = LayerMask.GetMask("Default");
 
             if (Mathf.Abs(delta.x) > Mathf.Abs(delta.y)) {
-                Debug.DrawRay(selfPosition, (Vector3)(Vector2.right * Mathf.Sign(delta.x)), Color.black, 20.0f);
-                var ray = (Vector3)(Vector2.right * Mathf.Sign(delta.x));
+                Vector3 ray = (Vector3)(Vector2.right * Mathf.Sign(delta.x));
                 RaycastHit2D collision = Physics2D.Raycast(selfPosition + (ray / 2), ray, 100, mask);
-                Debug.Log("Hit Something : " + collision.distance);
-
                 Vector2 dest = new Vector2(Mathf.Round(collision.distance) * Mathf.Sign(delta.x), 0);
-                StartCoroutine(this.Push((Vector2)selfPosition + dest));
+                // Debug.Log("Hit Something : " + collision.distance);
 
-                // if (!Physics2D.Raycast(selfPosition + (ray / 2), ray, 100, mask) && !this.pushing) {
-                //     StartCoroutine(this.Push((Vector2)selfPosition + new Vector2(Mathf.Sign(delta.x) * 1, 0)));
-                    
-                // }
+                StartCoroutine(this.Push((Vector2)selfPosition + dest));
             } else {
-                var ray = (Vector3)(Vector2.up * Mathf.Sign(delta.y));
-
+                Vector3 ray = (Vector3)(Vector2.up * Mathf.Sign(delta.y));
                 RaycastHit2D collision = Physics2D.Raycast(selfPosition + (ray / 2), ray, 100, mask);
-                Debug.Log("Hit Something : " + collision.distance);
-                Debug.DrawRay(selfPosition + (ray / 2), ray, Color.black, 20.0f);
                 Vector2 dest = new Vector2(0, Mathf.Round(collision.distance) * Mathf.Sign(delta.y));
+
+                // Debug.Log("Hit Something : " + collision.distance);
+                // Debug.DrawRay(selfPosition + (ray / 2), ray, Color.black, 20.0f);
+
                 StartCoroutine(this.Push((Vector2)selfPosition + dest));
-                // if (!Physics2D.Raycast(selfPosition + (ray / 2), ray, 100, mask) && !this.pushing) {
-                //     StartCoroutine(this.Push((Vector2)selfPosition + new Vector2(0, Mathf.Sign(delta.y) * 1)));
-                // }
             }
         }
     }
