b29c4e3055b65d238f6826fcdbc6a976a05440a2
Michael Ditto <michael.r.ditto@gmail.com>
2019-11-10 18:50:31 -0500

    UPT: Metrics for block pushing puzzle

---
+++ Assets/ResetTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetTrigger : MonoBehaviour {
+    void OnTriggerEnter2D(Collider2D other) {
+        PushableObject pushableObject = GameObject
+            .Find("PushableObject")
+            .GetComponent<PushableObject>();
+
+        pushableObject.ResetPuzzle();
+    }
+}
