7caf4dce7e7f9f65847fd3620e8af445ac89f404
Sean-Fisher <sean999fisher@gmail.com>
2019-12-01 19:21:40 -0500

    Disable bulletin board functionality

--- Assets/Scripts/Map/SceneSwitchHelper.cs
+++ Assets/Scripts/Map/SceneSwitchHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneSwitchHelper : MonoBehaviour
 {
@@ -13,5 +14,30 @@ public class SceneSwitchHelper : MonoBehaviour
 
     public void BeginGame() {
         SceneSwitcher.Singleton().SwitchToScene("Town Center");
+    }
+    
+    public void GotoShopScene()
+    {
+        SceneManager.LoadScene("Shop");
+    }
+
+    public void GotoForestRiverScene()
+    {
+        SceneManager.LoadScene("ForestRiver");
+    }
+
+    public void GotoForestScene()
+    {
+        SceneManager.LoadScene("ForestScene");
+    }
+
+    public void GotoSbRoomScene()
+    {
+        SceneManager.LoadScene("SbRoom");
+    }
+
+    public void GotoParkScene()
+    {
+        SceneManager.LoadScene("Park");
     }
 }
